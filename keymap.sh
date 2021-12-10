ede82b4f75d125537933364b0b2558c57e4fb905
terminaldweller <thabogre@gmail.com>
2021-12-10 17:48:56 +0000

    update for keymappings for xcape

--- keymap.sh
+++ keymap.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/sh
 
-xcape -e 'Control_L=Escape'
+#xcape -e 'Caps_Lock=Escape'
+setxkbmap -option 'caps:ctrl_modifier'
+xcape -e 'Caps_Lock=Escape' -t 100
 xcape -e 'Shift_L=Shift_L|bracketleft'
 xcape -e 'Shift_R=Shift_R|bracketright'
 xcape -e 'Alt_L=parenleft'
 xcape -e 'Alt_R=parenright'
+xcape -e 'Control_L=bracketleft'
+xcape -e 'Control_R=bracketright'
