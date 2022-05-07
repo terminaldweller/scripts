8fc30dbb1ef6afc2e0078ac14786d8ba52542348
terminaldweller <thabogre@gmail.com>
2022-05-07 20:39:26 +0430

    irssi update

--- irssi/irssi.sh
+++ irssi/irssi.sh
@@ -2,26 +2,25 @@
 
 mkdir -p ~/.irssi/scripts/autorun
 cd ~/.irssi/scripts/autorun
-wget https://scripts.irssi.org/scripts/nickcolor.pl
 wget https://scripts.irssi.org/scripts/adv_windowlist.pl
-wget https://scripts.irssi.org/scripts/timezones.pl
-wget https://scripts.irssi.org/scripts/seen.pl
-wget https://scripts.irssi.org/scripts/colorize_nicks.pl
-wget https://scripts.irssi.org/scripts/cubes.pl
-wget https://scripts.irssi.org/scripts/dim_nicks.pl
-wget https://scripts.irssi.org/scripts/hilite_url.pl
+wget https://scripts.irssi.org/scripts/dccstat.pl
+wget https://scripts.irssi.org/scripts/desktop_notify.pl
+wget https://raw.githubusercontent.com/terminaldweller/irssi-scripts/master/vim-mode/vim_mode.pl
+wget https://raw.githubusercontent.com/terminaldweller/irssi-scripts/master/irssi-logger/irssi_logger.pl
+wget https://raw.githubusercontent.com/terminaldweller/irssi-scripts/master/hilite-url/hilite_url.pl
+wget https://scripts.irssi.org/scripts/line_buffer.pl
 wget https://scripts.irssi.org/scripts/messages_bottom.pl
 wget https://scripts.irssi.org/scripts/nm2.pl
+wget https://scripts.irssi.org/scripts/rotator.pl
 wget https://scripts.irssi.org/scripts/savecmdhist.pl
-wget https://scripts.irssi.org/scripts/sb_position.pl
-wget https://scripts.irssi.org/scripts/trackbar.pl
+wget https://scripts.irssi.org/scripts/sbposition.pl
+wget https://scripts.irssi.org/scripts/sb_search.pl
 wget https://scripts.irssi.org/scripts/scriptassist.pl
-wget https://scripts.irssi.org/scripts/tordetect.pl
-wget https://scripts.irssi.org/scripts/usercount.pl
-# wget https://scripts.irssi.org/scripts/bitlbee_typing_notice.pl
+wget https://scripts.irssi.org/scripts/seen.pl
+wget https://scripts.irssi.org/scripts/timezones.pl
+wget https://scripts.irssi.org/scripts/trackbar.pl
 wget https://scripts.irssi.org/scripts/uberprompt.pl
-wget https://scripts.irssi.org/scripts/bitlbee_tab_completion.pl
-wget https://raw.githubusercontent.com/shabble/irssi-scripts/master/vim-mode/vim_mode.pl
+wget https://scripts.irssi.org/scripts/usercount.pl
 
 cd ~/scripts/irssi
 cp ./config ~/.irssi/config
